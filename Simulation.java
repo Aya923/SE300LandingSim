a99ec237839aa3e671015898d55841c419e3f2b0
Aya923 <isekia@my.erau.edu>
2025-03-03 15:39:55 -0500

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -1,10 +1,29 @@
 package SE300ProjectCode;
 
+import java.io.File;
+import java.util.Scanner;
 public class Simulation {
-
+	private double currentTime;
+	
 	public static void main(String[] args) {
-		
-
+		Simulation sim = new Simulationi();
+		sim.runSimulation();
 	}
+	
+	public void runSimulation(){
+	}
+	
+	//load simulation parameters from SimulationParameters
+	public void loadParameters(){
+	}	
+	
+	public void saveState(){
+	}
+	
+	public void displayResult(){
+		//Components: velocity, trajectories, states of the spacecraft, etc
+	}
+
+	
 
 }
